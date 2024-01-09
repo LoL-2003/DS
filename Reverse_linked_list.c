d7e41aec00e9ab2fdc772a1f8ee889edaa01cd22
LoL-2003 <148077211+LoL-2003@users.noreply.github.com>
2024-01-09 19:18:33 +0530

    Create Reverse_linked_list.c

---
+++ Reverse_linked_list.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+struct Node* reverse(struct Node* head) {
+    struct Node *prev = NULL, *current = head, *next = NULL;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+
+int main() {
+    // Creating a linked list: 1 -> 2 -> 3 -> 4
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+
+    printf("Original Linked List: ");
+    display(head);
+
+    head = reverse(head);
+
+    printf("Reversed Linked List: ");
+    display(head);
+
+    return 0;
+}
